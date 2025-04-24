caad2c23a43f06767a232181965c24c6779d9e47
kyungboshim <kyungbo.shim@neowiz.com>
2025-04-24 14:47:03 +0900

    4.2.0 배포

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AdiscopeMediaMax",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "AdiscopeMediaMax",
+            targets: ["AdiscopeMediaMaxTarget"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", exact: "13.1.0"),
+    ],
+    targets: [
+        .target(
+            name: "AdiscopeMediaMaxTarget",
+            dependencies: [
+                .target(name: "AdiscopeMediaMax"),
+                .product(name: "AppLovinSDK", package: "AppLovin-MAX-Swift-Package"),
+            ],
+            path: "Sources"
+        ),
+        .binaryTarget(
+            name: "AdiscopeMediaMax",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.2.0/AdiscopeMediaMax.zip",
+            checksum: "1d9862146d023b022db802a725f1f8373dfd65c3a64193b8be10924592fa57d4"
+        ),
+    ]
+)
